3706ae4662cd369a588dbe22be1aeac1d1c48e29
RWOverdijk <r.w.overdijk@gmail.com>
2015-12-07 16:03:15 +0100

    Build

--- dist/es6/decorator/repository.js
+++ dist/es6/decorator/repository.js
@@ -1,6 +1,6 @@
 import {OrmMetadata} from '../orm-metadata';
 
-export function repository (repository) {
+export function repository(repository) {
   return function (target) {
     OrmMetadata.forTarget(target).put('repository', repository);
   }
--- dist/system/entity.js
+++ dist/system/entity.js
@@ -23,12 +23,6 @@ System.register(['aurelia-validation', 'aurelia-framework', 'spoonx/aurelia-api'
         function Entity(validator, restClient) {
           _classCallCheck(this, _Entity);
 
-          Object.defineProperty(this, '__validator', {
-            value: validator,
-            writable: false,
-            enumerable: false
-          });
-
           Object.defineProperty(this, '__api', {
             value: restClient,
             writable: false,
@@ -41,9 +35,15 @@ System.register(['aurelia-validation', 'aurelia-framework', 'spoonx/aurelia-api'
             enumerable: false
           });
 
-          if (this.__meta.fetch('validation')) {
-            this.enableValidation();
+          if (!this.hasValidation()) {
+            return this;
           }
+
+          Object.defineProperty(this, '__validator', {
+            value: validator,
+            writable: false,
+            enumerable: false
+          });
         }
 
         _createClass(Entity, [{
@@ -108,6 +108,14 @@ System.register(['aurelia-validation', 'aurelia-framework', 'spoonx/aurelia-api'
         }, {
           key: 'enableValidation',
           value: function enableValidation() {
+            if (!this.hasValidation()) {
+              throw new Error('Entity not marked as validated. Did you forget the @validation() decorator?');
+            }
+
+            if (this.__validation) {
+              return this;
+            }
+
             Object.defineProperty(this, '__validation', {
               value: this.__validator.on(this),
               writable: false,
@@ -119,12 +127,20 @@ System.register(['aurelia-validation', 'aurelia-framework', 'spoonx/aurelia-api'
         }, {
           key: 'getValidation',
           value: function getValidation() {
+            if (!this.hasValidation()) {
+              return null;
+            }
+
+            if (!this.__validation) {
+              this.enableValidation();
+            }
+
             return this.__validation;
           }
         }, {
           key: 'hasValidation',
           value: function hasValidation() {
-            return !!this.__validation;
+            return !!this.__meta.fetch('validation');
           }
         }, {
           key: 'asObject',
