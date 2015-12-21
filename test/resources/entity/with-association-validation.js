b3f4764afb9bf2bcf1944d6a2d5b4ebe83b3a9fb
RWOverdijk <r.w.overdijk@gmail.com>
2015-12-21 15:56:10 +0100

    test(validator): Test the new validator

---
+++ test/resources/entity/with-association-validation.js
@@ -0,0 +1,15 @@
+import {validatedResource} from '../../../src/decorator/validated-resource';
+import {association} from '../../../src/decorator/association';
+import {Entity} from '../../../src/entity';
+import {ensure} from 'aurelia-validation';
+
+  @validatedResource('assoc-validation')
+export class WithAssociationValidation extends Entity {
+
+  @ensure(it => it.hasAssociation())
+  @association()
+  foo = null;
+
+  @association('custom')
+  bar = null;
+}
