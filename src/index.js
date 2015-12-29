1b3a8c789ff471db3a3c4537499d96b80c19c615
RWOverdijk <r.w.overdijk@gmail.com>
2015-12-29 15:15:48 +0100

    chore(project): Expose @name decorator

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ export {OrmMetadata} from './orm-metadata';
 export {EntityManager} from './entity-manager';
 export {association} from './decorator/association';
 export {resource} from './decorator/resource';
+export {name} from './decorator/name';
 export {repository} from './decorator/repository';
 export {validation} from './decorator/validation';
 export {validatedResource} from './decorator/validated-resource';
