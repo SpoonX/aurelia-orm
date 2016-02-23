893e3e8586900fc8ecb8346b103a9ff6afe78074
RWOverdijk <r.w.overdijk@gmail.com>
2016-02-23 16:07:34 +0100

    test(decorator): Added tests for type decorator

---
+++ test/decorator/type.spec.js
@@ -0,0 +1,11 @@
+import {WithType} from '../resources/entity/with-type';
+import {OrmMetadata} from '../../src/orm-metadata';
+
+describe('@type()', function () {
+  it('Should add types for properties on the entity.', function () {
+    expect(OrmMetadata.forTarget(WithType).fetch('types', 'created')).toEqual('datetime');
+    expect(OrmMetadata.forTarget(WithType).fetch('types', 'disabled')).toEqual('boolean');
+    expect(OrmMetadata.forTarget(WithType).fetch('types', 'age')).toEqual('integer');
+    expect(OrmMetadata.forTarget(WithType).fetch('types', 'titanic')).toEqual('float');
+  });
+});
