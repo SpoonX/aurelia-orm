3e70062e0f11d82bdadc80985a1eba625a152182
RWOverdijk <r.w.overdijk@gmail.com>
2016-02-23 16:07:15 +0100

    feat(decorator): Added type decorator

---
+++ src/decorator/type.js
@@ -0,0 +1,7 @@
+import {OrmMetadata} from '../orm-metadata';
+
+export function type(typeValue) {
+  return function(target, propertyName) {
+    OrmMetadata.forTarget(target.constructor).put('types', propertyName, typeValue);
+  };
+}
