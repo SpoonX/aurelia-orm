167029370241c078208ea38da062fe0b7d1d6efd
doktordirk <doktordirk@outlook.de>
2016-04-09 04:02:12 +0200

    chore(project): update dependencies

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ System.config({
   },
 
   map: {
-    "aurelia-api": "npm:aurelia-api@2.1.2",
+    "aurelia-api": "npm:aurelia-api@2.2.0",
     "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.3.1",
     "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.2.0",
     "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.1.2.0",
@@ -18,9 +18,9 @@ System.config({
     "extend": "npm:extend@3.0.0",
     "fetch": "github:github/fetch@0.11.0",
     "typer": "npm:typer@1.1.0",
-    "npm:aurelia-api@2.1.2": {
+    "npm:aurelia-api@2.2.0": {
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.2.0",
-      "aurelia-fetch-client": "npm:aurelia-fetch-client@1.0.0-beta.1.2.0",
+      "aurelia-fetch-client": "npm:aurelia-fetch-client@1.0.0-beta.1.2.1",
       "extend": "npm:extend@3.0.0",
       "qs": "npm:qs@6.1.0"
     },
@@ -36,7 +36,7 @@ System.config({
     },
     "npm:aurelia-loader@1.0.0-beta.1.2.0": {
       "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.1.2.0",
-      "aurelia-path": "npm:aurelia-path@1.0.0-beta.1.2.0"
+      "aurelia-path": "npm:aurelia-path@1.0.0-beta.1.2.1"
     },
     "npm:aurelia-metadata@1.0.0-beta.1.2.0": {
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.0"
@@ -57,7 +57,7 @@ System.config({
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.1.2.0",
       "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.1.2.0",
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.0",
-      "aurelia-path": "npm:aurelia-path@1.0.0-beta.1.2.0",
+      "aurelia-path": "npm:aurelia-path@1.0.0-beta.1.2.1",
       "aurelia-task-queue": "npm:aurelia-task-queue@1.0.0-beta.1.2.0"
     },
     "npm:aurelia-validation@0.6.6": {
