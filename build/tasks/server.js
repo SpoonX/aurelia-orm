82f5bd397d4a8c2d8cb28e8afcde754d8c3917ca
RWOverdijk <r.w.overdijk@gmail.com>
2016-04-23 15:04:33 +0200

    test(project): Added endpoints

--- build/tasks/server.js
+++ build/tasks/server.js
@@ -1,11 +1,37 @@
-var app        = require('express')();
-var bodyParser = require('body-parser');
-var cors       = require('cors');
+var app           = require('express')();
+var bodyParser    = require('body-parser');
+var cors          = require('cors');
+var autoIncrement = 1;
 var server;
 
 app.use(bodyParser.json());
 
 app.use(cors());
+
+app.post('/withassociations', function(req, res) {
+  var built = req.body;
+
+  built.id     = autoIncrement++;
+  built.bar.id = autoIncrement++;
+
+  res.send(built);
+});
+
+app.post('/foo', function(req, res) {
+  var built = req.body;
+
+  built.id = autoIncrement++;
+
+  res.send(built);
+});
+
+app.post('/custom', function(req, res) {
+  var built = req.body;
+
+  built.id = autoIncrement++;
+
+  res.send(built);
+});
 
 app.all('*', function(req, res) {
   res.send({
@@ -21,7 +47,7 @@ module.exports = {
   start: function(done) {
     server = app.listen(1927, done);
   },
-  stop: function(cb) {
+  stop : function(cb) {
     server.close(cb);
   }
 };
