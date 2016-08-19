6d9b3f5aa816c00ae657b50451ab8668b8210b8b
VMBindraban <vijay@sweebr.com>
2016-08-19 10:37:10 +0200

    test(tests): add tests

--- test/entity.spec.js
+++ test/entity.spec.js
@@ -327,6 +327,10 @@ describe('Entity', function() {
       expect(entity.isNew()).toBe(true);
       entity.setData({idTag: 667}).markClean();
       expect(entity.isNew()).toBe(false);
+      entity.setData({id: null});
+      expect(entity.isNew()).toBe(true);
+      entity.setData({id: false});
+      expect(entity.isNew()).toBe(true);
     });
   });
 
