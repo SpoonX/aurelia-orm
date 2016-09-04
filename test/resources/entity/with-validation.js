1389d6e2da5c75aecb477b5d4d8708ed084af6c0
doktordirk <doktordirk@outlook.de>
2016-09-04 13:30:33 +0200

    chore(test): removed obsolete tests

--- test/resources/entity/with-validation.js
+++
@@ -1,9 +0,0 @@
-import {validatedResource} from '../../../src/decorator/validated-resource';
-import {Entity} from '../../../src/entity';
-
-@validatedResource()
-export class WithValidation extends Entity {
-
-  //@ensure(it => it.isNotEmpty().hasLengthBetween(3, 20))
-  foo = null;
-}
