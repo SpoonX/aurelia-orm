1c266cecf3ef23968000f9fa32554ae4543d1ca9
doktordirk <doktordirk@outlook.de>
2016-09-04 08:35:32 +0200

    feat(aurelia-orm): add customRule hasCollectionAssociation

--- src/aurelia-orm.js
+++ src/aurelia-orm.js
@@ -9,10 +9,29 @@ export function configure(aurelia, configCallback) {
 
   configCallback(entityManagerInstance);
 
+  // add custom rules
   ValidationRules.customRule(
     'hasAssociation',
     value => !!((value instanceof Entity && typeof value.id === 'number') || typeof value === 'number'),
     `\${$displayName} must be an association.`    // eslint-disable-line quotes
+  );
+
+  ValidationRules.customRule(
+    'hasCollectionAssociation',
+    values => {
+      if (!Array.isArray(values)) {
+        return false;
+      }
+
+      for (let value of values) {
+        if ( !((value instanceof Entity && typeof value.id === 'number') || typeof value === 'number')) {
+          return false;
+        }
+      }
+
+      return true;
+    },
+    `\${$displayName} must be a collection association.`    // eslint-disable-line quotes
   );
 
   aurelia.globalResources('./component/association-select');
