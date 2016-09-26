324ef38c7490ada691b03e49f8318ed3d7fef179
doktordirk <doktordirk@outlook.de>
2016-09-26 18:29:07 +0200

    feat(property-decorators): make property decorators (type & association) default observable
    Fixes an issue with babel's property decorator, which prevents aurelia from binding properly

--- src/decorator/association.js
+++ src/decorator/association.js
@@ -1,7 +1,8 @@
 import {OrmMetadata} from '../orm-metadata';
+import {observable} from 'aurelia-binding';
 
 /**
- * Associate a property with an entity (toOne) or a collection (toMany)
+ * Associate a property with an entity (toOne) or a collection (toMany). Also and makes it observable
  *
  * @param {undefined|String|Object} associationData undefined={entity:propertyName}, String={entity:String}, Object={entity: String, collection: String}
  *
@@ -10,7 +11,9 @@ import {OrmMetadata} from '../orm-metadata';
  * @decorator
  */
 export function association(associationData) {
-  return function(target, propertyName) {
+  return function(target, propertyName, descriptor) {
+    observable()(target, propertyName, descriptor);
+
     if (!associationData) {
       associationData = {entity: propertyName};
     } else if (typeof associationData === 'string') {
