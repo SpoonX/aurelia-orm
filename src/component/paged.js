be045574ed9afe3bb2766097e8de50068572c88f
Jason Carreira <jason.carreira@perspectivepartners.com>
2017-07-06 17:45:17 -0400

    feat(paged): add loading flag

--- src/component/paged.js
+++ src/component/paged.js
@@ -10,130 +10,130 @@ import {resolvedView} from 'aurelia-view-manager';
 export class Paged {
 
   // https://github.com/aurelia/templating/issues/73, you still had to set `data` on .two-way when global
-@bindable({defaultBindingMode: bindingMode.twoWay}) data    = [];
-@bindable({defaultBindingMode: bindingMode.twoWay}) loading = false;
-@bindable({defaultBindingMode: bindingMode.twoWay}) page    = 1;
-@bindable({defaultBindingMode: bindingMode.twoWay}) error;
-@bindable criteria
-@bindable repository                                        = null;
-@bindable resource
-@bindable limit                                             = 30;
+  @bindable({defaultBindingMode: bindingMode.twoWay}) data    = [];
+  @bindable({defaultBindingMode: bindingMode.twoWay}) loading = false;
+  @bindable({defaultBindingMode: bindingMode.twoWay}) page    = 1;
+  @bindable({defaultBindingMode: bindingMode.twoWay}) error;
+  @bindable criteria;
+  @bindable repository                                        = null;
+  @bindable resource;
+  @bindable limit                                             = 30;
 
-constructor(entityManager) {
-  this.entityManager = entityManager;
-}
-
-/**
- * Attach to view
- */
-attached() {
-  if (!this.page) {
-    this.page = 1;
+  constructor(entityManager) {
+    this.entityManager = entityManager;
   }
 
-  if (!this.criteria) {
-    this.criteria = {};
+  /**
+   * Attach to view
+   */
+  attached() {
+    if (!this.page) {
+      this.page = 1;
+    }
+
+    if (!this.criteria) {
+      this.criteria = {};
+    }
+
+    this.reloadData();
   }
 
-  this.reloadData();
-}
-
-/**
- * Reload data
- */
-reloadData() {
-  this.getData();
-}
-
-/**
- * Check if the element property has changed
- *
- * @param  {string}      property New element property
- * @param  {string|{}}   newVal New value
- * @param  {string|{}}   oldVal Old value
- *
- * @return {boolean}
- */
-isChanged(property, newVal, oldVal) {
-  return !this[property] || !newVal || (newVal === oldVal);
-}
-
-/**
- * Changed page handler
- *
- * @param  {integer} newVal New page value
- * @param  {integer} oldVal Old page value
- */
-pageChanged(newVal, oldVal) {
-  if (this.isChanged('resource', newVal, oldVal)
-    || this.isChanged('criteria', newVal, oldVal)
-  ) {
-    return;
+  /**
+   * Reload data
+   */
+  reloadData() {
+    this.getData();
   }
 
-  this.reloadData();
-}
-
-/**
- * Changed resource handler
- *
- * @param  {{}} newVal New resource value
- * @param  {{}} oldVal Old resource value
- */
-resourceChanged(newVal, oldVal) {
-  if (this.isChanged('resource', newVal, oldVal)) {
-    return;
+  /**
+   * Check if the element property has changed
+   *
+   * @param  {string}      property New element property
+   * @param  {string|{}}   newVal New value
+   * @param  {string|{}}   oldVal Old value
+   *
+   * @return {boolean}
+   */
+  isChanged(property, newVal, oldVal) {
+    return !this[property] || !newVal || (newVal === oldVal);
   }
 
-  this.reloadData();
-}
+  /**
+   * Changed page handler
+   *
+   * @param  {integer} newVal New page value
+   * @param  {integer} oldVal Old page value
+   */
+  pageChanged(newVal, oldVal) {
+    if (this.isChanged('resource', newVal, oldVal)
+      || this.isChanged('criteria', newVal, oldVal)
+    ) {
+      return;
+    }
 
-/**
- * Changed criteria handler
- *
- * @param  {{}} newVal New criteria value
- * @param  {{}} oldVal Old criteria value
- */
-criteriaChanged(newVal, oldVal) {
-  if (this.isChanged('criteria', newVal, oldVal)) {
-    return;
+    this.reloadData();
   }
 
-  this.reloadData();
-}
+  /**
+   * Changed resource handler
+   *
+   * @param  {{}} newVal New resource value
+   * @param  {{}} oldVal Old resource value
+   */
+  resourceChanged(newVal, oldVal) {
+    if (this.isChanged('resource', newVal, oldVal)) {
+      return;
+    }
 
-/**
- * Changed resource handler
- *
- * @param  {string} resource New resource value
- */
-resourceChanged(resource) {
-  if (!resource) {
-    logger.error(`resource is ${typeof resource}. It should be a string or a reference`);
+    this.reloadData();
   }
 
-  this.repository = this.entityManager.getRepository(resource);
-}
+  /**
+   * Changed criteria handler
+   *
+   * @param  {{}} newVal New criteria value
+   * @param  {{}} oldVal Old criteria value
+   */
+  criteriaChanged(newVal, oldVal) {
+    if (this.isChanged('criteria', newVal, oldVal)) {
+      return;
+    }
 
-/**
- * Get data from repository
- */
-getData() {
-  let criteria = JSON.parse(JSON.stringify(this.criteria));
+    this.reloadData();
+  }
 
-  criteria.skip  = (this.page * this.limit) - this.limit;
-  criteria.limit = this.limit;
-  this.error     = null;
-  this.loading   = true;
+  /**
+   * Changed resource handler
+   *
+   * @param  {string} resource New resource value
+   */
+  resourceChanged(resource) {
+    if (!resource) {
+      logger.error(`resource is ${typeof resource}. It should be a string or a reference`);
+    }
 
-  this.repository.find(criteria, true)
-    .then(result => {
-    this.data    = result;
-  this.loading = false;
-})
-.catch(error => {
-    this.error   = error;
-  this.loading = false;
-});
-}
+    this.repository = this.entityManager.getRepository(resource);
+  }
+
+  /**
+   * Get data from repository
+   */
+  getData() {
+    let criteria = JSON.parse(JSON.stringify(this.criteria));
+
+    criteria.skip  = (this.page * this.limit) - this.limit;
+    criteria.limit = this.limit;
+    this.error     = null;
+    this.loading   = true;
+
+    this.repository.find(criteria, true)
+      .then(result => {
+        this.data    = result;
+        this.loading = false;
+      })
+      .catch(error => {
+        this.error   = error;
+        this.loading = false;
+      });
+  }
 }
